c4381475427aaa3fcfa924f00c374983070ee4d5
Tamara556 <murazyan.tamara5566@gmail.com>
2025-09-24 17:22:33 +0400

    home page added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,34 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: `
-    <div class="home-container">
-      <h1>Welcome to OpenEdu</h1>
-      <p>Your educational platform is ready!</p>
-    </div>
-  `,
-  styles: [`
-    .home-container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      min-height: 100vh;
-      text-align: center;
-      padding: 2rem;
-    }
-    
-    h1 {
-      font-size: 3rem;
-      margin-bottom: 1rem;
-      color: #333;
-    }
-    
-    p {
-      font-size: 1.2rem;
-      color: #666;
-    }
-  `]
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 }
